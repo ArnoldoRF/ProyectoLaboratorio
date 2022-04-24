c70b211028fdc9bbc05228340469905cbd35bb8b
ArnoldoRF <76766109+ArnoldoRF@users.noreply.github.com>
2022-04-24 19:41:08 -0400

    Add files via upload

---
+++ src/main/GestionSocial.java
@@ -0,0 +1,14 @@
+
+package main;
+
+
+public class GestionSocial {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
