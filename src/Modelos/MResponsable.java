cfc6038e4daaf222dcaf9757ba5b630376cbf1b7
ArnoldoRF <arnol.jose.rodriz.fre@gmail.com>
2022-04-25 01:13:40 -0400

    Creado MResponsable

---
+++ src/Modelos/MResponsable.java
@@ -0,0 +1,16 @@
+
+package Modelos;
+
+public class MResponsable extends MPersona {
+
+    private String codigoFundacion;
+    
+    public String getCodigoFundacion() {
+        return codigoFundacion;
+    }
+
+    public void setCodigoFundacion(String codigoFundacion) {
+        this.codigoFundacion = codigoFundacion;
+    }
+
+}
