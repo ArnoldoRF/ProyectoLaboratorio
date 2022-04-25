7489dbe7ec84a27fddfa981e11556b6cff634fa4
jo1201 <josuefaneittes@gmail.com>
2022-04-25 08:16:55 -0400

    SolicitudCRUD

--- src/Modelos/CRUD/SolicitudCRUD.java
+++ src/Modelos/CRUD/SolicitudCRUD.java
@@ -5,6 +5,8 @@ import Modelos.MSolicitud;
 import Modelos.Database.ConexionDB;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
 /**
  *
  * @author josue
@@ -20,13 +22,14 @@ public class SolicitudCRUD extends ConexionDB{
         
         conectar();
         try(PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setInt(1, solicitud.getCodigo());
+            ps.setInt(1, Integer.parseInt(solicitud.getCodigo()));
             ps.setString(2, solicitud.getCedulaResponsable());
             ps.setString(3, solicitud.getCedulaBeneficiario());
-            ps.setInt(4, solicitud.getCodigoFundacion());
+            ps.setInt(4, Integer.parseInt(solicitud.getCodigoFundacion()));
             ps.setString(5, solicitud.getPrioridad());
             ps.setDate(6, solicitud.getFechaCreacion());
-            ps.setString(4, "A");
+            ps.setDate(7, solicitud.getFechaAprovacion());
+            ps.setString(8, "A");
             ps.execute();
         }
         catch(Exception e) {
@@ -35,20 +38,25 @@ public class SolicitudCRUD extends ConexionDB{
         desconectar();
     }
     
-    public boolean consultar(MServicio servicio) {
+    public boolean consultar(MSolicitud solicitud) {
         boolean encontrado = false;
         
-        String sql = "SELECT * FROM public.servicio "
-                + "WHERE cod_servicio=?";
+        String sql = "SELECT * FROM public.solicitud "
+                + "WHERE cod_solic=?";
         
         conectar();
         try(PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setInt(1, Integer.parseInt(servicio.getCodigo()));
+            ps.setInt(1, Integer.parseInt(solicitud.getCodigo()));
             
             ResultSet rs = ps.executeQuery();
             if(rs.next()) {
-                servicio.setNombre(rs.getString("nomb_servicio"));
-                servicio.setCosto(Double.parseDouble(rs.getString("costo")));
+                solicitud.setCedulaResponsable(rs.getNString("cedula_res"));
+                solicitud.setCedulaBeneficiario(rs.getNString("cedula_benef"));
+                solicitud.setCodigoFundacion(rs.getNString("cod_fund"));
+                solicitud.setPrioridad(rs.getNString("prioridad"));
+                solicitud.setFechaCreacion(rs.getDate("fecha_creacion"));
+                solicitud.setFechaCreacion(rs.getDate("fecha_aprob"));
+             
                 encontrado = true;
             }
         }
@@ -59,15 +67,19 @@ public class SolicitudCRUD extends ConexionDB{
         return encontrado;
     }
     
-    public void actualizar(MServicio servicio) {
-        String sql = "UPDATE public.servicio SET nomb_servicio=?, costo=? "
-                + "WHERE cod_servicio=?";
+    public void actualizar(MSolicitud solicitud) {
+        String sql = "UPDATE public.solicitud SET cedula_res=?, cedula_benef=?, cod_fund=?, prioridad=?, fecha_creacion=?, fecha_aprob=?, estatus=? "
+                + "WHERE cod_solic=?";
         
         conectar();
         try(PreparedStatement ps = con.prepareStatement(sql)) {
-            ps.setString(1, servicio.getNombre());
-            ps.setDouble(2, servicio.getCosto());
-            ps.setInt(3, Integer.parseInt(servicio.getCodigo()));
+            ps.setString(1, solicitud.getCedulaResponsable());
+            ps.setString(2, solicitud.getCedulaBeneficiario());
+            ps.setInt(3, Integer.parseInt(solicitud.getCodigoFundacion()));
+            ps.setString(4, solicitud.getPrioridad());
+            ps.setDate(5, solicitud.getFechaCreacion());
+            ps.setDate(6, solicitud.getFechaAprovacion());
+            ps.setString(7, "A");
             ps.execute();
         }
         catch(Exception e) {
@@ -76,14 +88,14 @@ public class SolicitudCRUD extends ConexionDB{
         desconectar();
     }
     
-    public void eliminar(MServicio servicio) {
-        String sql = "UPDATE public.servicio SET estatus=? "
-                + "WHERE codigo=?";
+    public void eliminar(MSolicitud solicitud) {
+        String sql = "UPDATE public.solicitud SET estatus=? "
+                + "WHERE cod_solic=?";
         
         conectar();
         try(PreparedStatement ps = con.prepareStatement(sql)) {
             ps.setString(1, "E");
-            ps.setInt(1, Integer.parseInt(servicio.getCodigo()));
+            ps.setInt(1, Integer.parseInt(solicitud.getCodigo()));
             ps.execute();
         }
         catch(Exception e) {
--- src/Modelos/MSolicitud.java
+++ src/Modelos/MSolicitud.java
@@ -1,21 +1,28 @@
 
 package Modelos;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.sql.Date;
 
 public class MSolicitud {
-    private int codigo;
+    private String codigo;
     private String cedulaResponsable;
     private String cedulaBeneficiario;
-    private int codigoFundacion;
+    private String codigoFundacion;
     private String prioridad;
     private Date fechaCreacion;
     private Date fechaAprobacion;
     
-    public MSolicitud() {}
+    public MSolicitud() {
+        codigo="";
+        cedulaResponsable="";
+        cedulaBeneficiario="";
+        codigoFundacion= "";
+        prioridad="";
+    }
     
-    public MSolicitud(int codigo, String cedulaResponsable, String cedulaBeneficiario,
-            int codigoFundacion, String prioridad, Date fechaCreacion, Date fechaAprobacion) {
+    public MSolicitud(String codigo, String cedulaResponsable, String cedulaBeneficiario,
+            String codigoFundacion, String prioridad, Date fechaCreacion, Date fechaAprobacion) {
         
         this.codigo = codigo;
         this.cedulaResponsable = cedulaResponsable;
@@ -26,11 +33,11 @@ public class MSolicitud {
         this.fechaCreacion = fechaCreacion;
     }
 
-    public int getCodigo() {
+    public String getCodigo() {
         return codigo;
     }
 
-    public void setCodigo(int codigo) {
+    public void setCodigo(String codigo) {
         this.codigo = codigo;
     }
 
@@ -58,11 +65,11 @@ public class MSolicitud {
         this.cedulaBeneficiario = cedulaBeneficiario;
     }
 
-    public int getCodigoFundacion() {
+    public String getCodigoFundacion() {
         return codigoFundacion;
     }
 
-    public void setCodigoFundacion(int codigoFundacion) {
+    public void setCodigoFundacion(String codigoFundacion) {
         this.codigoFundacion = codigoFundacion;
     }
 
