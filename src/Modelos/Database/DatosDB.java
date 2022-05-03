132d25026ffda4ac34cbc10034f1797dfe1eb939
Paulinaana <azuaje43@gmail.com>
2022-05-02 23:03:05 -0400

    Nombres

--- src/Modelos/Database/DatosDB.java
+++ src/Modelos/Database/DatosDB.java
@@ -1,4 +1,10 @@
-
+/*
+Equipo Nro 1 
+Arnoldo Rodríguez 27.479.958
+Ana Calles 28.363.935
+Fernando Montilla 26.182.328
+Josue Faneittes 27.436.537
+*/
 package Modelos.Database;
 
 public class DatosDB {
