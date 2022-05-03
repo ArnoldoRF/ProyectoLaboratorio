d7aff10509e594e540754a418c95be1951bd5781
jo1201 <josuefaneittes@gmail.com>
2022-05-02 23:18:41 -0400

    Agregar BD

--- src/Modelos/Database/DatosDB.java
+++ src/Modelos/Database/DatosDB.java
@@ -9,7 +9,7 @@ package Modelos.Database;
 
 public class DatosDB {
     
-    protected String database = "postgres",
+    protected String database = "Gobernacion",
                           url = "jdbc:postgresql://localhost:5432/" + database,
                          user = "postgres",
                          pass = "admin";
