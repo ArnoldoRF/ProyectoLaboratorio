77473bfb793892ef08262435b6ed5a5729b581b6
ArnoldoRF <76766109+ArnoldoRF@users.noreply.github.com>
2022-06-06 14:04:52 -0400

    Commit

--- src/utilidades/GestorConexion.java
+++ src/utilidades/GestorConexion.java
@@ -11,8 +11,6 @@ package utilidades;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class GestorConexion {
     
